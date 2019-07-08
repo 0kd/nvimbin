ca871e83d9ee737ba7a7f53184a5faf88bd92607
tsuchiura <39905164+tsuchiura@users.noreply.github.com>
2019-07-08 15:06:04 +0900

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,7 @@
+export HTTP_PROXY=ユーザー名:パスワード@proxyのURL:ポート番号
+export HTTPS_PROXY=ユーザー名:パスワード@proxyのURL:ポート番号
+export http_proxy=ユーザー名:パスワード@proxyのURL:ポート番号
+export https_proxy=ユーザー名:パスワード@proxyのURL:ポート番号
+
+proxy-user = "ユーザー名:パスワード"
+proxy = http://proxyのURL:ポート番号
